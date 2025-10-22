fbd0efc135157af030c6e6df9397fda8432cfa02
DONSUTHAN <donsuthan123@gmail.com>
2025-10-22 15:56:18 +0530

    my first commit
    first commit

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the number :");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number is an even");
+}
+else
+{
+printf("the number is an odd");
+}
+}
---
+++ revno.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("Enter the number : ");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d ",i);
+}
+}
